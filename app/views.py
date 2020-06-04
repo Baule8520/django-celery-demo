3f4e61edfdfcc378703a7b122fd02dd0805a857a
Paul Z <paul@zechs.de>
2020-06-04 12:48:38 +0200

    Demo System

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .tasks import sleepy
+
+# Create your views here.
+
+def index(request):
+    sleepy.delay(10)
+    return HttpResponse('<h1>Task is Done!</h1>')
